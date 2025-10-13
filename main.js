89732fedf1c6f702b347abe1be5622be101b4f3a
Arthur Santos <Arthur.s.vale@gmail.com>
2025-10-13 14:01:17 -0300

    Update main.js
    Adicionado duas novas classes

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-function Carro(nomeMarca, nomeModelo, anoFabricacao, corVeiculo, tipoCombustivel, tipoMotorizacao, estadoCarro) {
+function Carro(nomeMarca, nomeModelo, anoFabricacao, corVeiculo, tipoCombustivel) {
     this.marca = nomeMarca; //atributo para definir a marca do carro Ex: Ford
     this.modelo = nomeModelo; //atributo para qual é o carro Ex: Ford Ka
     this.ano = anoFabricacao; //atributo dizendo quando o carro foi fabricado
     this.cor = corVeiculo;
-    this.combustivel = tipoCombustivel; //atributo que define que tipo de combustivel o carro usa Gasolina, Etanol ou Diesel
+}
+
+function Motor(tipoMotorizacao, estadoCarro) {
     this.motorizacao = tipoMotorizacao; //atributo que diz quanto a potencia do carro se é 1.0 ou 2.0
-    this.ligado = estadoCarro; //atributo que define o estado inicial do carro LIGADO ou DESLIGADO
+    this.estadoCarro = estadoCarro; //atributo que define o estado inicial do carro LIGADO ou DESLIGADO
 
     this.ligar = function() {
         if (this.ligado === 'desligado') {
@@ -24,12 +26,41 @@ function Carro(nomeMarca, nomeModelo, anoFabricacao, corVeiculo, tipoCombustivel
     }
 }
 
-//Definindo as instancias do Objeto
-const carroArthur = new Carro('Ford', 'Ford Ka', 2011, 'preto', 'combustivel', '1.6', 'desligado');
-const carroFernanda = new Carro('BYD', 'Dolphin Sear', 2024, 'Azul', 'Eletrico', '95cv', 'ligado');
-const carroGuilherme = new Carro('Chevrolet', 'Onix', 2026, 'Vermelho Scarlet', '1.0', 'ligado');
+function TanqueCombustivel(capacidade, quantidade) {
+    this.capacidadeMaxima = capacidade;
+    this.nivel = quantidade;
 
-//Mostando os atributos e chamado o método do objeto
-console.log(carroArthur, carroArthur.ligar());
-console.log(carroFernanda, carroFernanda.desligar());
-console.log(carroGuilherme, carroGuilherme.ligar());
+    this.verificaTanque = function() {
+        if (this.nivel === 0) {
+            console.log("O tanque está vázio e preciso abastecer!!");
+        } else if (this.nivel < this.capacidadeMaxima * 0.25){
+            console.log("O tanque está abaixo dos 25% é preciso abastecer")
+        } else if (this.nivel < this.capacidadeMaxima) {
+            console.log("O tanque está cheio porém necessário realizar o abstecimento");
+        } else {
+            console.log("O tanque está cheio");
+        }
+    }
+}
+
+//Definindo as instancias do Objeto
+const carroArthur = new CarroCombustao('Ford', 'Ford Ka', 2011, 'preto', 'gasolina');
+const motorCarroArthur = new Motor("1.6", "desligado");
+const tanqueCarroArthur = new TanqueCombustivel(30, 10);
+console.log(carroArthur);
+console.log(motorCarroArthur.ligar());
+console.log(tanqueCarroArthur.verificaTanque());
+
+const carroFernanda = new Carro('BYD', 'Dolphin Sear', 2024, 'Azul', 'Eletrico');
+const motorCarroFernanda = new Motor("2.0", "ligado");
+const tanqueCarroFernanda = new TanqueCombustivel(50, 50);
+console.log(carroFernanda);
+console.log(motorCarroFernanda.ligar());
+console.log(tanqueCarroFernanda.verificaTanque());
+
+const carroGuilherme = new Carro('Chevrolet', 'Onix', 2026, 'Vermelho Scarlet');
+const motorCarroGuilherme = new Motor("1.0", "ligado");
+const tanqueCarroGuilherme = new TanqueCombustivel(65, 0);
+console.log(carroGuilherme);
+console.log(motorCarroGuilherme.desligar());
+console.log(tanqueCarroGuilherme.verificaTanque());
