4cb85cb1da6bb740d3684de38bacdc42f562770d
Arthur Santos <Arthur.s.vale@gmail.com>
2025-10-14 11:25:29 -0300

    Atualizado o código da main

--- main.js
+++ main.js
@@ -1,66 +1,74 @@
-function Carro(nomeMarca, nomeModelo, anoFabricacao, corVeiculo, tipoCombustivel) {
-    this.marca = nomeMarca; //atributo para definir a marca do carro Ex: Ford
-    this.modelo = nomeModelo; //atributo para qual é o carro Ex: Ford Ka
-    this.ano = anoFabricacao; //atributo dizendo quando o carro foi fabricado
-    this.cor = corVeiculo;
+/* Classe Mãe */
+function Carro(marca, cor, anoFabricacao, tipoDeCombustivel) {
+    this.marca = marca;
+    this.cor = cor;
+    this.anoFabricacao = anoFabricacao;
+    this.tipoDeCombustivel = tipoDeCombustivel;
 }
 
-function Motor(tipoMotorizacao, estadoCarro) {
-    this.motorizacao = tipoMotorizacao; //atributo que diz quanto a potencia do carro se é 1.0 ou 2.0
-    this.estadoCarro = estadoCarro; //atributo que define o estado inicial do carro LIGADO ou DESLIGADO
+/* Classe filha */
+function Hb20(marca, cor, anoFabricacao, tipoDeCombustivel, valor, estado) {
+    Carro.call(this, marca, cor, anoFabricacao, tipoDeCombustivel);
 
-    this.ligar = function() {
-        if (this.ligado === 'desligado') {
-            console.log("O carro foi ligado!");
-        } else {
-            console.log("O carro já está ligado!");
-        }
-    }
+    this.valor = valor;
+    this.estado = estado
 
-    this.desligar = function () {
-        if (this.ligado === 'ligado') {
-            console.log("O carro foi desligado!");
+    //Método para verificar o estado inicial do carro
+    this.estadoCarro = function () {
+        if (this.estado === "desligado") {
+            console.log('O carro foi ligado');
         } else {
-            console.log("O carro já está desligado!");
+            console.log("O carro já se encontra ligado");
         }
     }
 }
 
-function TanqueCombustivel(capacidade, quantidade) {
-    this.capacidadeMaxima = capacidade;
-    this.nivel = quantidade;
+/* Segunda classe filha */
+function Nivus(marca, cor, anoFabricacao, tipoDeCombustivel, valor, estado) {
+    Carro.call(this, marca, cor, anoFabricacao, tipoDeCombustivel);
 
-    this.verificaTanque = function() {
-        if (this.nivel === 0) {
-            console.log("O tanque está vázio e preciso abastecer!!");
-        } else if (this.nivel < this.capacidadeMaxima * 0.25){
-            console.log("O tanque está abaixo dos 25% é preciso abastecer")
-        } else if (this.nivel < this.capacidadeMaxima) {
-            console.log("O tanque está cheio porém necessário realizar o abstecimento");
+    this.valor = valor;
+    this.estado = estado;
+
+    //Método para verificar o estado inicial do carro
+    this.estadoCarro = function () {
+        if (this.estado === "desligado") {
+            console.log('O carro foi ligado');
         } else {
-            console.log("O tanque está cheio");
+            console.log("O carro já se encontra ligado");
         }
     }
 }
 
-//Definindo as instancias do Objeto
-const carroArthur = new CarroCombustao('Ford', 'Ford Ka', 2011, 'preto', 'gasolina');
-const motorCarroArthur = new Motor("1.6", "desligado");
-const tanqueCarroArthur = new TanqueCombustivel(30, 10);
-console.log(carroArthur);
-console.log(motorCarroArthur.ligar());
-console.log(tanqueCarroArthur.verificaTanque());
+/* Terceira classe filha */
+function FastBack(marca, cor, anoFabricacao, tipoDeCombustivel, valor, estado) {
+    Carro.call(this, marca, cor, anoFabricacao, tipoDeCombustivel);
 
-const carroFernanda = new Carro('BYD', 'Dolphin Sear', 2024, 'Azul', 'Eletrico');
-const motorCarroFernanda = new Motor("2.0", "ligado");
-const tanqueCarroFernanda = new TanqueCombustivel(50, 50);
-console.log(carroFernanda);
-console.log(motorCarroFernanda.ligar());
-console.log(tanqueCarroFernanda.verificaTanque());
+    this.valor = valor;
+    this.estado = estado;
 
-const carroGuilherme = new Carro('Chevrolet', 'Onix', 2026, 'Vermelho Scarlet');
-const motorCarroGuilherme = new Motor("1.0", "ligado");
-const tanqueCarroGuilherme = new TanqueCombustivel(65, 0);
-console.log(carroGuilherme);
-console.log(motorCarroGuilherme.desligar());
-console.log(tanqueCarroGuilherme.verificaTanque());
+    //Método para verificar o estado inicial do carro
+    this.estadoCarro = function () {
+        if (this.estado === "desligado") {
+            console.log('O carro foi ligado');
+        } else {
+            console.log("O carro já se encontra ligado");
+        }
+    }
+}
+
+/* Instâncias das classes */
+const carroMaria = new Hb20("Hyundai", "Prata", "2025", "Gasolina", 82.000, "desligado");
+const carroJose = new Nivus("Volkswagen", "Azul", "2024", "Total Flex", 120.000, "ligado");
+const carroFernando = new FastBack("Fiat", "Vermelho", "2025", "Hybrido", 165.000, "ligado");
+
+/* Apresentando no console as informações das classes */
+console.log(`A Maria possui um ${carroMaria.marca} que é um modelo HB20 de cor ${carroMaria.cor}, Ano ${carroMaria.anoFabricacao} e sua tabela fipe é ${carroMaria.valor} mil`);
+console.log(carroMaria.estadoCarro());
+
+console.log(`O José possui um ${carroJose.marca} que é um modelo HB20 de cor ${carroJose.cor}, Ano ${carroJose.anoFabricacao} e sua tabela fipe é ${carroJose.valor} mil`);
+console.log(carroJose.estadoCarro());
+
+console.log(`O Fernando possui um ${carroFernando.marca} que é um modelo HB20 de cor ${carroFernando.cor}, Ano ${carroFernando.anoFabricacao} e sua tabela fipe é ${carroFernando.valor} mil`);
+console.log(carroFernando.estadoCarro());
+
